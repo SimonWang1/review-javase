34e77c961a56938e5df53ef09f095ae34ba1dec0
Wangsx <wangsixxx@sina.com>
2017-12-21 23:16:54 +0800

    update object

---
+++ src/com/wsx/chapterdemo5_1/Bird.java
@@ -0,0 +1,7 @@
+package com.wsx.chapterdemo5_1;
+
+public class Bird {
+	public void fly() {
+		System.out.println("I can fly in the sky...");
+	}
+}
---
+++ src/com/wsx/chapterdemo5_1/ConstructorOverload.java
@@ -0,0 +1,20 @@
+package com.wsx.chapterdemo5_1;
+
+public class ConstructorOverload {
+	public static void main(String[] args) {
+		//通过无参构造器创建对象
+		ConstructorOverload co1 = new ConstructorOverload();
+		ConstructorOverload co2 = new ConstructorOverload("网络是怎样连接的", 233);
+		System.out.println(co1.name + "  " + co1.count);
+		System.out.println(co2.name + "  " + co2.count);
+	}
+	public String name;
+	public int count;
+	public ConstructorOverload() {
+	}
+	//提供两个带参构造器，进行对象初始化
+	public ConstructorOverload(String name, int count) {
+		this.name = name;
+		this.count = count;
+	}
+}
