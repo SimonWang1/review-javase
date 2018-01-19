d998e765ecd0e4c3082856110e3417a3dc2fa69d
Wangsx <wangsixxx@sina.com>
2018-01-19 17:29:30 +0800

    update sum array

--- src/com/wsx/algorithm/SumArray.java
+++ src/com/wsx/algorithm/SumArray.java
@@ -3,9 +3,21 @@ package com.wsx.algorithm;
 public class SumArray {
 	public static void main(String[] args) {
 		// 数组求和
-		int[] intArray= {1, 2, 3, 4, 5};
+		int[] intArray = {1, 2, 3, 4, 5};
 		System.out.println(sumArray1(intArray));
 		System.out.println(sumArray2(intArray, intArray.length));
+		/*
+			运行过程
+			5 + sumArray2(intArray, 4)
+			4 + sumArray2(intArray, 3)
+			3 + sumArray2(intArray, 2)
+			2 + sumArray2(intArray, 1)
+			1 + sumArray2(intArray, 0)
+			到达基线条件
+			1 + 2 = 3
+			3 + 3 = 6
+		
+		*/
 	}
 	
 	// 普通实现
@@ -20,8 +32,8 @@ public class SumArray {
 	// 递归实现
 	public static int sumArray2(int[] intArray, int len) {
 		// 基线条件
-		if(len == 1) {
-			return intArray[0];
+		if(len == 0) {
+			return 0;
 		}
 		// 递归条件
 		else {
