5c4a661d3d164f45bf7fb8a4f278de2b35d3f0a1
Wangsx <wangsixxx@sina.com>
2018-01-23 17:02:25 +0800

    learn quick sort

--- src/com/wsx/algorithm/QuickSort.java
+++ src/com/wsx/algorithm/QuickSort.java
@@ -2,27 +2,40 @@ package com.wsx.algorithm;
 
 public class QuickSort {
 	public static void main(String[] args) {
-		int[] array = {2};
+		int[] array = {10, 4, 3, 2, 5};
 		quickSort(array);
 	}
 	
-	public static void quickSort(int[] array) {
-//		int less, greater;
-		if(array.length < 2) {
-			for(int i : array) {
-				System.out.print(i);
+	public static void quickSort(int[] a) {
+		sort(a, 0, a.length - 1);
+		for(int i : a) {
+			System.out.print(i + " ");
+		}
+	}
+	
+	public static void sort(int[] a, int low, int high) {
+		int start = low;
+		int end = high;
+		int pivot = a[low];
+		while(end > start) {
+			while(end > start && a[end] >= pivot)
+				end--;
+			if(a[end] <= pivot) {
+				int temp = a[end];
+				a[end] = a[start];
+				a[start] = temp;
+			}
+			while(end > start && a[start] <= pivot)
+				start++;
+			if(a[start] >= pivot) {
+				int temp = a[start];
+				a[start] = a[end];
+				a[end] = temp;
 			}
 		}
-		else {
-			int pivot = array[0];
-			for(int i = 2; i < array.length; i++) {
-				if(array[i] <= pivot) {
-//					less = array[i];
-				}
-				else {
-//					greater = array[i];
-				}
-			}
-		}
+		if(start > low) 
+			sort(a, low, start - 1);
+		if(end < high) 
+			sort(a, end + 1, high);
 	}
 }
