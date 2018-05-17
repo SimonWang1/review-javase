84283e35f858d3b54caff9b62310632136f8d902
Wangsx <wangsixxx@sina.com>
2018-05-17 19:40:11 +0800

    update SelectSort

--- src/com/wsx/algorithm/SelectionSort.java
+++ src/com/wsx/algorithm/SelectionSort.java
@@ -9,21 +9,21 @@ public class SelectionSort {
 			System.out.print(i + " ");
 		}
 		System.out.println();
-		// 遍历数组
+		// 依次遍历数组
 		for (int i = 0; i < intArray.length; i++) {
-			// 声明临时变量存储选中索引和元素值
+			// 声明临时变量存储外循环元素的索引和元素值
 			int index = i;
 			int element = intArray[i];
-			// 比较选中元素之后的元素值
+			// 依次遍历外循环元素之后的元素
 			for (int j = i + 1; j < intArray.length; j++) {
-				// 如果选中元素小于后者
+				// 如果后面的元素比较小
 				if (intArray[j] < element) {
 					// 将对应元素的索引和值存储在临时变量中
 					index = j;
 					element = intArray[j];
 				}
 			}
-			// 交换元素，比较下一个
+			// 交换最小元素，比较下一个
 			int temp = intArray[i];
 			intArray[i] = intArray[index];
 			intArray[index] = temp;
